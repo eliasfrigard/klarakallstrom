3a2fcd55b5d0e56150b35c6b4aaa3441a0b4e4b1
Elias Frigård <elias.frigard@tvtools.fi>
2023-11-12 17:55:08 +0200

    disable image modal

--- components/ImageLayout.js
+++ components/ImageLayout.js
@@ -1,17 +1,17 @@
 import Image from 'next/image'
 import AnimateIn from '../components/AnimateIn'
 
-const Hero = ({ image, index, handleOnClick }) => {
+const ImageLayout = ({ image, index, handleOnClick }) => {
   return (
-    <>
-      <AnimateIn onClick={handleOnClick} delay={index * 1000} className={`relative h-full w-full max-h-[700px] aspect-square delay-[${index * 1000}}ms]`} >
+    <a href={image}>
+      <AnimateIn delay={index * 1000} className={`relative h-full w-full max-h-[700px] aspect-square delay-[${index * 1000}}ms]`} >
         <Image alt={image} src={image + '?w=1000&h=1000'} fill className={`object-cover object-center rounded shadow`} />
 
         {/* IMAGE OVERLAY */}
-        <div className='absolute bg-secondary-500 w-full h-full z-10 opacity-20 hover:opacity-0 duration-300 grayscale'></div>
+        <div className='absolute cursor-pointer bg-secondary-500 w-full h-full z-10 opacity-20 hover:opacity-0 duration-300 grayscale'></div>
       </AnimateIn >
-    </>
+    </a>
   )
 }
 
-export default Hero
+export default ImageLayout
