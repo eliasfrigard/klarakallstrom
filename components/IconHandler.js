a4506c0e7c5fa5a1103d161e01f95bf6b606aa0f
Elias Frigård <elias.frigard@tvtools.fi>
2023-11-12 20:57:08 +0200

    disable "noIcons"

--- components/IconHandler.js
+++ components/IconHandler.js
@@ -18,7 +18,7 @@ const IconHandler = ({
 }) => {
   const [mailLink, setMailLink] = React.useState(null)
   const [addressLink, setAddressLink] = React.useState(null)
-  const [noIcons, setNoIcons] = React.useState(false)
+  // const [noIcons, setNoIcons] = React.useState(false)
 
   React.useEffect(() => {
     if (!address) {
@@ -36,19 +36,18 @@ const IconHandler = ({
     }
   }, [email, websiteName])
 
-  React.useEffect(() => {
-    setNoIcons(prev => (
-      !email
-      && !facebook
-      && !instagram
-      && !spotify
-      && !youTube
-      && !website
-      && !tickets
-      && !address
-      && !websiteName
-    ));
-  }, [email, facebook, instagram, spotify, youTube, website, tickets, address, websiteName])
+  // React.useEffect(() => {
+  //   setNoIcons(prev => (
+  //     && !facebook
+  //     && !instagram
+  //     && !spotify
+  //     && !youTube
+  //     && !website
+  //     && !tickets
+  //     && !address
+  //     && !websiteName
+  //   ));
+  // }, [email, facebook, instagram, spotify, youTube, website, tickets, address, websiteName])
 
   const LinkIcon = ({ children, href, tooltip }) => {
     return (
@@ -65,7 +64,7 @@ const IconHandler = ({
     )
   }
 
-  if (noIcons) return ''
+  // if (noIcons) return ''
 
   return (
     <div className={`flex text-2xl justify-center items-center gap-5 z-10 ${className}`}>
