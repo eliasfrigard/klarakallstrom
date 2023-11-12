5efcdec0f05d595cf5fb814623b0ea9dc30692bc
Elias Frigård <elias.frigard@tvtools.fi>
2023-11-12 20:03:07 +0200

    hero image sizes

--- components/Hero.js
+++ components/Hero.js
@@ -3,20 +3,25 @@ import Image from 'next/image'
 import AnimateIn from '../components/AnimateIn'
 
 const Hero = ({ children, className, desktopImg, mobileImg, altText, overlay = true }) => {
+  const desktopImageUrl = desktopImg ? desktopImg + '?w=800' : mobileImg + '?w=800'
+  const mobileImageUrl = mobileImg ? mobileImg + '?w=600' : desktopImg + '?w=600'
+
   return (
     <AnimateIn className={className}>
       <div id='hero' className='relative h-screen -mt-[75px] flex justify-center items-center shadow-lg'>
         <Image
           alt={altText}
-          src={desktopImg || mobileImg}
+          src={desktopImageUrl}
           fill
+          sizes="(min-width: 768px) 80vw, 100vw"
           className={`${mobileImg && 'hidden md:block'} object-cover`}
         />
 
         <Image
           alt={altText}
-          src={mobileImg || desktopImg}
+          src={mobileImageUrl}
           fill
+          sizes="(min-width: 768px) 80vw, 100vw"
           className='md:hidden object-cover object-bottom'
         />
 
