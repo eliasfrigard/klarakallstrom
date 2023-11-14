30d0baa4ae6f5bd423bb622c5d844e444cb9a8cc
Elias Frigård <elias.frigard@tvtools.fi>
2023-11-14 20:59:25 +0200

    start home page animations earlier

--- pages/index.js
+++ pages/index.js
@@ -22,51 +22,50 @@ export default function Home({ pageTitle, hero, mobileHero, youTubeLink, altYouT
       >
         <div className="centerContent text-center flex-col gap-3 md:gap-2 text-white tracking-widest font-cursive">
           <div className="flex text-4xl sm:text-7xl lg:text-9xl tracking-wider opacity-80">
-            <AnimateIn animationType="zoom" classes="delay-[1000ms]"><h1>K</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[1100ms]"><h1>l</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[1200ms]"><h1>a</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[1300ms]"><h1>r</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[1400ms]"><h1>a</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[500ms]"><h1>K</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[600ms]"><h1>l</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[700ms]"><h1>a</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[800ms]"><h1>r</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[900ms]"><h1>a</h1></AnimateIn>
             <h1 className="mx-1 md:mx-2"></h1>
-            <AnimateIn animationType="zoom" classes="delay-[1500ms]"><h1>K</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[1600ms]"><h1>ä</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[1700ms]"><h1>l</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[1800ms]"><h1>l</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[1900ms]"><h1>s</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[2000ms]"><h1>t</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[2100ms]"><h1>r</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[2200ms]"><h1>ö</h1></AnimateIn>
-            <AnimateIn animationType="zoom" classes="delay-[2300ms]"><h1>m</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1000ms]"><h1>K</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1100ms]"><h1>ä</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1200ms]"><h1>l</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1300ms]"><h1>l</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1400ms]"><h1>s</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1500ms]"><h1>t</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1600ms]"><h1>r</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1700ms]"><h1>ö</h1></AnimateIn>
+            <AnimateIn animationType="zoom" classes="delay-[1800ms]"><h1>m</h1></AnimateIn>
           </div>
 
           <div className="flex opacity-70">
             <div className="text lg:text-2xl font-medium leading-none tracking-widest font-josefin flex">
-              <AnimateIn animationType="zoom" classes="delay-[1750ms]"><h1>C</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1250ms]"><h1>C</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1275ms]"><h1>e</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1300ms]"><h1>l</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1325ms]"><h1>l</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1350ms]"><h1>i</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1375ms]"><h1>s</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1400ms]"><h1>t</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1425ms]"><h1>,</h1></AnimateIn>
+              <h1 className="mx-[3px] md:mx-1.5"></h1>
+              <AnimateIn animationType="zoom" classes="delay-[1450ms]"><h1>A</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1475ms]"><h1>r</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1500ms]"><h1>t</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1525ms]"><h1>i</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1550ms]"><h1>s</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1575ms]"><h1>t</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1600ms]"><h1>,</h1></AnimateIn>
+              <h1 className="mx-[3px] md:mx-1.5"></h1>
+              <AnimateIn animationType="zoom" classes="delay-[1625ms]"><h1>C</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1650ms]"><h1>o</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1675ms]"><h1>m</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1700ms]"><h1>p</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1725ms]"><h1>o</h1></AnimateIn>
+              <AnimateIn animationType="zoom" classes="delay-[1750ms]"><h1>s</h1></AnimateIn>
               <AnimateIn animationType="zoom" classes="delay-[1775ms]"><h1>e</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[1800ms]"><h1>l</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[1825ms]"><h1>l</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[1850ms]"><h1>i</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[1875ms]"><h1>s</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[1900ms]"><h1>t</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[1925ms]"><h1>,</h1></AnimateIn>
-              <h1 className="mx-[3px] md:mx-1.5"></h1>
-              <AnimateIn animationType="zoom" classes="delay-[1950ms]"><h1>A</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[1975ms]"><h1>r</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2000ms]"><h1>t</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2025ms]"><h1>i</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2050ms]"><h1>s</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2075ms]"><h1>t</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2100ms]"><h1>,</h1></AnimateIn>
-              <h1 className="mx-[3px] md:mx-1.5"></h1>
-              <AnimateIn animationType="zoom" classes="delay-[2125ms]"><h1>C</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2150ms]"><h1>o</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2175ms]"><h1>m</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2200ms]"><h1>p</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2225ms]"><h1>o</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2250ms]"><h1>s</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2275ms]"><h1>e</h1></AnimateIn>
-              <AnimateIn animationType="zoom" classes="delay-[2300ms]"><h1>r</h1></AnimateIn>
-
+              <AnimateIn animationType="zoom" classes="delay-[1800ms]"><h1>r</h1></AnimateIn>
             </div>
           </div>
 
